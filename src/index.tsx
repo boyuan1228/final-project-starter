2402b4d4b5e5380d4068a5b027ebe89e65875b5f
oscarwyx <oscarwyx@udel.edu>
2021-10-07 17:23:07 -0400

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-//import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import Counter from  "./counter";
 
 ReactDOM.render(
-    <h1>World! Hello</h1>,
-    document.getElementById("root"));
+    <React.StrictMode>
+        <h1>Welcome!</h1>
+        <Counter />
+    </React.StrictMode>,
+
+    document.getElementById("root")
+);
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
